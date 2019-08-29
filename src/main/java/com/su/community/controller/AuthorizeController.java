14e6b9e6aaebbd01fc73f51e2b9cd8065bd42104
Supreme_Nick <dy122537@163.com>
2019-08-29 14:18:38 +0800

    实现持久化登录状态获取

--- src/main/java/com/su/community/controller/AuthorizeController.java
+++ src/main/java/com/su/community/controller/AuthorizeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
@@ -35,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -47,15 +49,18 @@ public class AuthorizeController {
         System.out.println(githubUser.getName());
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf((githubUser.getId())));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            response.addCookie(new Cookie("token",token));
+            //将user信息插入数据库
             userMapper.insert(user);
             //登陆成功，写cookie和session
             //request.getSession()可以得到HttpSession类型的对象
-            request.getSession().setAttribute("user", githubUser);
+//            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         } else {
             //登录失败，重新登录
--- src/main/java/com/su/community/mapper/UserMapper.java
+++ src/main/java/com/su/community/mapper/UserMapper.java
@@ -3,10 +3,16 @@ package com.su.community.mapper;
 import com.su.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    //@Param表示当形参是类可以不用@Param，不是类要用这个注解
+    User findByToken(@Param("token") String token);
 }
