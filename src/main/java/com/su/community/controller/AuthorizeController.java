182f44ad8250ead212a464e07ec668fcb3a77b36
Supreme_Nick <dy122537@163.com>
2019-08-30 21:27:37 +0800

    小修改

--- src/main/java/com/su/community/controller/AuthorizeController.java
+++ src/main/java/com/su/community/controller/AuthorizeController.java
@@ -55,7 +55,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf((githubUser.getId())));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            user.setAvatarUrl(githubUser.getAvatar_url());
+            user.setAvatarUrl(githubUser.getAvatarUrl());
             response.addCookie(new Cookie("token", token));
             //将user信息插入数据库
             userMapper.insert(user);
