fb755bb3150d67897e0ef95a7855cff053016c38
Supreme_Nick <dy122537@163.com>
2019-08-30 11:32:06 +0800

    将发布页面的数据存入数据库

--- src/main/java/com/su/community/controller/PublishController.java
+++ src/main/java/com/su/community/controller/PublishController.java
@@ -1,12 +1,66 @@
 package com.su.community.controller;
 
+import com.su.community.mapper.QuestionMapper;
+import com.su.community.mapper.UserMapper;
+import com.su.community.model.Question;
+import com.su.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request,
+                            Model model) {
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                }
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question qusetion = new Question();
+        qusetion.setTitle(title);
+        qusetion.setDescription(description);
+        qusetion.setTag(tag);
+        qusetion.setCreator(user.getId());
+        qusetion.setGmtCreate(System.currentTimeMillis());
+        qusetion.setGmtModified(qusetion.getGmtCreate());
+        questionMapper.create(qusetion);
+        return "redirect:/";
     }
 }
